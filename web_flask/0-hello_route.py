a68e62c8b8c1ef6051301d5d925da228751cb6e4
datlearner <kundaimuchemwa456@gmail.com>
2024-05-16 08:25:09 +0200

    added

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """ returns hello for a request to the / route """
 
